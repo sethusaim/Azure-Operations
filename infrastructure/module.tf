7433a69ef8afc36ca824d3c35b6511b7bf8cd26d
sethusaim <sethusaim@gmail.com>
2022-10-05 16:29:20 +0530

    test

--- infrastructure/module.tf
+++ infrastructure/module.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "scaniatfstate"
     storage_account_name = "scaniatfstate"
-    container_name       = "tfstate"
+    container_name       = "tfstate" 
     key                  = "prod.terraform.tfstate"
   }
 }
@@ -15,19 +15,19 @@ module "resource_group" {
   source = "./scania_resource_group"
 }
 
-# module "virtual_machine" {
-#   source = "./scania_vm"
-#   depends_on = [
-#     module.resource_group
-#   ]
-# }
+module "virtual_machine" {
+  source = "./scania_vm"
+  depends_on = [
+    module.resource_group
+  ]
+}
 
-# module "io_files_container" {
-#   source = "./scania_io_files_container"
-#   depends_on = [
-#     module.resource_group
-#   ]
-# }
+module "blob_container" {
+  source = "./scania_io_files_container"
+  depends_on = [
+    module.resource_group
+  ]
+}
 
 module "acr" {
   source = "./scania_acr"
