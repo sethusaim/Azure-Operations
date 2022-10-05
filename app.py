49ceb5fa33e7d78bce1306da0abd59a4ec379489
sethusaim <sethusaim@gmail.com>
2022-10-05 14:38:41 +0530

    test app commit 1

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def hello():
     return "Hello, World!"
 
 
+@app.route("/first")
+def first():
+    return "This is first page"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
