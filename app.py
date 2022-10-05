1f1dc3b9fdbba131b90b2993c22f0a68cbdb63dc
sethusaim <sethusaim@gmail.com>
2022-10-05 14:06:46 +0530

    test commit 2

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+
+@app.route("/first")
+def first():
+    return "This is first page"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
